18c231ae36d96d6c636c93035a49be9f4c08e38c
h00k86 <h00k86@protonmail.com>
2026-02-01 13:34:48 +0100

    [ADD] initialize some structure

--- Makefile
+++ Makefile
@@ -1,3 +1,18 @@
 
 
+obj-m := main.o
+
+PWD := $(shell pwd)
+KDIR := $(PWD)/linux-6.17
+
+
+
+all:
+	$(MAKE) -C $(KDIR) M=$(PWD)  modules
+
+
+clean: 
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
+
+
 
--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
+
+
+#include "include/config.h"
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-int __init my_init(void) { return 0; }
 
-void __exit my_exit(void) {}
+static int __init my_init(void) { 
+	
+	printk(KERN_INFO "module inserted");	
+	return 0;
+}
+
+static void __exit my_exit(void) {
+
+	printk(KERN_INFO "module removed");	
+}
+
 
 module_init(my_init);
 module_exit(my_exit);
 
-MODULE_LICENCE();
-MODULE_AUTHOR();
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("h00k86");
+MODULE_DESCRIPTION("A rootkit ");
